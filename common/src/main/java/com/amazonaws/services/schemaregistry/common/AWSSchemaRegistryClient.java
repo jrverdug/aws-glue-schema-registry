c29365d7abf4df3c2212a433cad02de12d6dcefa
jverdugo <jverdugo@twilio.com>
2025-09-30 10:51:47 -0700

    update pom files, remove unused glue methods

--- common/src/main/java/com/amazonaws/services/schemaregistry/common/AWSSchemaRegistryClient.java
+++ common/src/main/java/com/amazonaws/services/schemaregistry/common/AWSSchemaRegistryClient.java
@@ -17,44 +17,29 @@ package com.amazonaws.services.schemaregistry.common;
 
 import com.amazonaws.services.schemaregistry.common.configs.GlueSchemaRegistryConfiguration;
 import com.amazonaws.services.schemaregistry.exception.AWSSchemaRegistryException;
-import com.amazonaws.services.schemaregistry.utils.AWSSchemaRegistryConstants;
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.ImmutableMap;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.awscore.AwsRequestOverrideConfiguration;
-import software.amazon.awssdk.core.ApiName;
-import software.amazon.awssdk.core.SdkRequest;
 import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
-import software.amazon.awssdk.core.interceptor.Context;
-import software.amazon.awssdk.core.interceptor.ExecutionAttributes;
-import software.amazon.awssdk.core.interceptor.ExecutionInterceptor;
 import software.amazon.awssdk.core.retry.RetryPolicy;
 import software.amazon.awssdk.http.urlconnection.ProxyConfiguration;
 import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.glue.model.GetSchemaVersionResponse;
 import software.amazon.awssdk.services.glue.model.MetadataKeyValuePair;
-import software.amazon.awssdk.services.glue.model.QuerySchemaVersionMetadataResponse;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3ClientBuilder;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.GetObjectTaggingRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectTaggingResponse;
 import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
 import software.amazon.awssdk.services.s3.model.Tag;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Optional;
-import java.util.StringJoiner;
 import java.util.UUID;
 
 /**
@@ -82,7 +67,6 @@ public class AWSSchemaRegistryClient {
         this.glueSchemaRegistryConfiguration = glueSchemaRegistryConfiguration;
         ClientOverrideConfiguration overrideConfiguration = ClientOverrideConfiguration.builder()
                 .retryPolicy(retryPolicy)
-                .addExecutionInterceptor(new UserAgentRequestInterceptor())
                 .build();
         UrlConnectionHttpClient.Builder urlConnectionHttpClientBuilder = UrlConnectionHttpClient.builder();
         if (glueSchemaRegistryConfiguration.getProxyUrl() != null) {
@@ -197,55 +181,6 @@ public class AWSSchemaRegistryClient {
         }
     }
 
-    private GetSchemaVersionRequest getSchemaVersionRequest(String schemaVersionId) {
-        GetSchemaVersionRequest getSchemaVersionRequest = GetSchemaVersionRequest.builder()
-                .schemaVersionId(schemaVersionId).build();
-        return getSchemaVersionRequest;
-    }
-
-    private void validateSchemaVersionResponse(GetSchemaVersionResponse schemaVersionResponse, String schemaVersionId) {
-        if (schemaVersionResponse == null || schemaVersionResponse.schemaVersionId() == null) {
-            String message = String.format("Schema definition is not present for the schema id = %s", schemaVersionId);
-            throw new AWSSchemaRegistryException(message);
-        }
-    }
-
-    private UUID returnSchemaVersionIdIfAvailable(GetSchemaByDefinitionResponse response) {
-        if (response.schemaVersionId() != null
-                && response.statusAsString().equals(AWSSchemaRegistryConstants.SchemaVersionStatus.AVAILABLE.toString())) {
-            return UUID.fromString(response.schemaVersionId());
-        } else {
-            String msg = String.format("Schema Found but status is %s", response.statusAsString());
-            throw new AWSSchemaRegistryException(msg);
-        }
-    }
-
-    /**
-     * Create a request to get a schema using the schema definition and the schema name.
-     *
-     * @param schemaDefinition Schema Definition
-     * @param schemaName       Schema Name
-     * @return                 GetSchemaByDefinitionRequest object
-     */
-    public GetSchemaByDefinitionRequest buildGetSchemaByDefinitionRequest(String schemaDefinition, String schemaName) {
-        return buildGetSchemaByDefinitionRequest(schemaDefinition, schemaName, glueSchemaRegistryConfiguration.getRegistryName());
-    }
-
-    /**
-     * Create a request to get a schema using the schema definition and the schema name.
-     *
-     * @param schemaDefinition Schema Definition
-     * @param schemaName       Schema Name
-     * @param registryName     Registry name
-     * @return                 GetSchemaByDefinitionRequest object
-     */
-    public GetSchemaByDefinitionRequest buildGetSchemaByDefinitionRequest(String schemaDefinition, String schemaName,
-                                                                          String registryName) {
-        GetSchemaByDefinitionRequest request = GetSchemaByDefinitionRequest.builder()
-                .schemaId(getSchemaIdRequestObject(schemaName, registryName))
-                .schemaDefinition(schemaDefinition).build();
-        return request;
-    }
 
     /**
      * Create a schema - NOT SUPPORTED in S3 mode.
@@ -297,95 +232,6 @@ public class AWSSchemaRegistryClient {
         );
     }
 
-    private GetSchemaVersionResponse transformToGetSchemaVersionResponse(RegisterSchemaVersionResponse registerSchemaVersionResponse) {
-        return GetSchemaVersionResponse.builder()
-                .schemaVersionId(registerSchemaVersionResponse.schemaVersionId())
-                .status(registerSchemaVersionResponse.status())
-                .status(registerSchemaVersionResponse.statusAsString())
-                .versionNumber(registerSchemaVersionResponse.versionNumber())
-                .build();
-    }
-
-    private CreateSchemaRequest getCreateSchemaRequestObject(String schemaName, String dataFormat, String schemaDefinition) {
-        return CreateSchemaRequest
-                .builder()
-                .dataFormat(DataFormat.valueOf(dataFormat))
-                .description(glueSchemaRegistryConfiguration.getDescription())
-                .registryId(RegistryId.builder().registryName(glueSchemaRegistryConfiguration.getRegistryName()).build())
-                .schemaName(schemaName)
-                .schemaDefinition(schemaDefinition)
-                .compatibility(glueSchemaRegistryConfiguration.getCompatibilitySetting())
-                .tags(glueSchemaRegistryConfiguration.getTags())
-                .build();
-    }
-
-    private RegisterSchemaVersionRequest getRegisterSchemaVersionRequest(String schemaDefinition, String schemaName) {
-        return RegisterSchemaVersionRequest
-                .builder()
-                .schemaDefinition(schemaDefinition)
-                .schemaId(getSchemaIdRequestObject(schemaName, glueSchemaRegistryConfiguration.getRegistryName()))
-                .build();
-    }
-
-    private SchemaId getSchemaIdRequestObject(@NonNull String schemaName, @NonNull String registryName) {
-        return SchemaId
-                .builder()
-                .schemaName(schemaName)
-                .registryName(registryName)
-                .build();
-    }
-
-    private GetSchemaVersionRequest getGetSchemaVersionRequest(String schemaVersionId) {
-        return GetSchemaVersionRequest
-                .builder()
-                .schemaVersionId(schemaVersionId)
-                .build();
-    }
-
-    /**
-     * Get schema version response of asynchronous operation.
-     *
-     * @return Schema version.
-     */
-    private GetSchemaVersionResponse waitForSchemaEvolutionCheckToComplete(GetSchemaVersionRequest getSchemaVersionRequest) {
-
-        GetSchemaVersionResponse response;
-
-        try {
-            int retries = 0;
-
-            Thread.sleep(MAX_WAIT_INTERVAL);
-
-            do {
-                response = client.getSchemaVersion(getSchemaVersionRequest);
-
-                if (AWSSchemaRegistryConstants.SchemaVersionStatus.AVAILABLE.toString()
-                        .equals(response.statusAsString())) {
-                    return response;
-                } else if (!AWSSchemaRegistryConstants.SchemaVersionStatus.PENDING.toString()
-                        .equals(response.statusAsString())) {
-                    throw new AWSSchemaRegistryException(String.format("Schema evolution check failed. "
-                                                                       + "schemaVersionId %s is in %s status.",
-                                                                       getSchemaVersionRequest.schemaVersionId(),
-                                                                       response.statusAsString()));
-                }
-
-            } while (retries++ < MAX_ATTEMPTS - 1);
-
-            if (retries >= MAX_ATTEMPTS && !AWSSchemaRegistryConstants.SchemaVersionStatus.AVAILABLE.toString()
-                    .equals(response.statusAsString())) {
-                throw new AWSSchemaRegistryException(String.format("Retries exhausted for schema evolution check for "
-                                                                   + "schemaVersionId = %s",
-                                                                   getSchemaVersionRequest.schemaVersionId()));
-            }
-        } catch (Exception ex) {
-            String message =
-                    String.format("Exception occurred, while performing schema evolution check for schemaVersionId = "
-                                  + "%s", getSchemaVersionRequest.schemaVersionId());
-            throw new AWSSchemaRegistryException(message, ex);
-        }
-        return response;
-    }
 
     /**
      * Put metadata to schema version - NOT SUPPORTED in S3 mode.
@@ -415,22 +261,6 @@ public class AWSSchemaRegistryClient {
         );
     }
 
-    private PutSchemaVersionMetadataRequest createPutSchemaVersionMetadataRequest(UUID schemaVersionId, MetadataKeyValuePair metadataKeyValuePair) {
-        return PutSchemaVersionMetadataRequest
-                .builder()
-                .schemaVersionId(schemaVersionId.toString())
-                .metadataKeyValue(metadataKeyValuePair)
-                .build();
-    }
-
-    private MetadataKeyValuePair createMetadataKeyValuePair(Map.Entry<String, String> metadataEntry) {
-        return MetadataKeyValuePair
-                .builder()
-                .metadataKey(metadataEntry.getKey())
-                .metadataValue(metadataEntry.getValue())
-                .build();
-    }
-
     /**
      * Query metadata for schema version - NOT SUPPORTED in S3 mode.
      *
@@ -443,13 +273,6 @@ public class AWSSchemaRegistryClient {
             "querySchemaVersionMetadata is not supported in S3 mode. " +
             "Use S3 GetObjectTagging API to read object tags instead."
         );
-    }
-
-    private QuerySchemaVersionMetadataRequest createQuerySchemaVersionMetadataRequest(UUID schemaVersionId) {
-        return QuerySchemaVersionMetadataRequest
-                .builder()
-                .schemaVersionId(schemaVersionId.toString())
-                .build();
     }
 
     /**
@@ -466,59 +289,6 @@ public class AWSSchemaRegistryClient {
         );
     }
 
-    /**
-     * AWS SDK Request interceptor that adds additional data to the UserAgent of Glue API requests.
-     */
-    @VisibleForTesting
-    protected class UserAgentRequestInterceptor implements ExecutionInterceptor {
-        private static final String ONE = "1";
-        private static final String ZERO = "0";
-
-        @Override
-        public SdkRequest modifyRequest(Context.ModifyRequest context, ExecutionAttributes executionAttributes) {
-            if (!(context.request() instanceof GlueRequest)) {
-                //Only applies to Glue requests.
-                return context.request();
-            }
-
-            GlueRequest request = (GlueRequest) context.request();
-            AwsRequestOverrideConfiguration overrideConfiguration =
-                request.overrideConfiguration().map(config ->
-                    config
-                        .toBuilder()
-                        .addApiName(getApiName())
-                        .build())
-                    .orElse((AwsRequestOverrideConfiguration.builder()
-                        .addApiName(getApiName())
-                        .build()));
-
-            return request.toBuilder().overrideConfiguration(overrideConfiguration).build();
-        }
-
-        private ApiName getApiName() {
-            return ApiName.builder()
-                .version(com.amazonaws.services.schemaregistry.common.MavenPackaging.VERSION)
-                .name(buildUserAgentSuffix())
-                .build();
-        }
-
-        private String buildUserAgentSuffix() {
-            Map<String, String> userAgentSuffixItems = ImmutableMap.of(
-                "autoreg", glueSchemaRegistryConfiguration.isSchemaAutoRegistrationEnabled() ? ONE : ZERO,
-                "compress", glueSchemaRegistryConfiguration.getCompressionType().equals(
-                    AWSSchemaRegistryConstants.COMPRESSION.ZLIB) ? ONE : ZERO,
-                "secdeser", glueSchemaRegistryConfiguration.getSecondaryDeserializer() != null ? ONE : ZERO,
-                "app", glueSchemaRegistryConfiguration.getUserAgentApp()
-            );
-
-            StringJoiner userAgentSuffix = new StringJoiner(":");
-
-            userAgentSuffixItems
-                .forEach((key, value) -> userAgentSuffix.add(key + "/" + value));
-
-            return userAgentSuffix.toString();
-        }
-    }
 
     /**
      * Build S3 key for schema.avsc file using configured prefix
